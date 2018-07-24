6e9ca533a274913b3b2af0c00e645136ab9df903
cdROma <mrv24ru@yandex.ru>
2018-07-25 01:06:05 +0300

    server-http

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require( 'express' );
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use( bodyParser.urlencoded( {extended: true } ) );
+app.use( bodyParser.json() );
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.set('port', (process.env.PORT || 8080));
+app.listen(app.get('port'), function() {
+    console.log('Сервер запущен на порту '+app.get('port'));
+});
